d4af8b8986375014a3d88d2ff178efe115c2384f
RishiVarsha <prishivarsha03@gmail.com>
2024-01-16 12:22:23 +0530

    first commit

---
+++ About.js
@@ -0,0 +1,59 @@
+function home() {
+    addEventListener("click", function() {
+        window.location.href = "index.html";
+    })
+}
+
+function linkedin() {
+    addEventListener("click", function() {
+        window.location.href = window.open("https://www.linkedin.com/in/poranki-rishi-varsha-67b5a4249/", "_blank");
+    })
+}
+
+function discord() {
+    addEventListener("click", function() {
+        window.location.href = window.open("https://discord.com/users/RishiVarsha#2045", "_blank");
+    })
+}
+
+function gmail() {
+    addEventListener("click", function() {
+        window.location.href = window.open("mailto:prishivarsha03@gmail.com", "_blank");
+    })
+}
+
+function outlook() {
+    addEventListener("click", function() {
+        window.location.href = window.open("mailto:varsharishi2003@outlook.com", "_blank");
+    })
+}
+
+function github() {
+    addEventListener("click", function() {
+        window.location.href = window.open("https://github.com/RishiVarsha?tab=repositories", "_blank");
+    })
+}
+
+function Home() {
+    addEventListener("click", function() {
+        window.location.href = "index.html";
+    });
+}
+
+function Courses() {
+    addEventListener("click", function() {
+        window.location.href = "Courses.html";
+    });
+}
+
+function Notes() {
+    addEventListener("click", function() {
+        window.location.href = "Notes.html";
+    });
+}
+
+function About() {
+    addEventListener("click", function() {
+        window.location.href = "About.html";
+    });
+}
